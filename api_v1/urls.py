bdca52740ef68f43b3d23ec54896b932b3a63afa
Kanzhar Ryskulov <ryskulovkanzar@gmail.com>
2026-07-23 11:17:50 +0600

    Создал функционал для суммирования двух чисел

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from api_v1.views import json_echo_view , add
+from api_v1.views import json_echo_view, add, subtract
 
 urlpatterns = [
     path("echo/", json_echo_view, name="list"),
     path("add/", add),
-    # path("subtract/", subtract),
+    path("subtract/", subtract),
     # path("multiply/", multiply),
     # path("divide/", divide)
 ]
